5cf9275675873f8fb75f1592780642afcc7970d0
ChrisRoland <chrisebuberoland@gmail.com>
2025-06-14 11:03:30 +0100

    feat: features branch
    Created, moved to and added all files to a features branch

--- src/pages/TodoDetail.jsx
+++
@@ -1,6 +0,0 @@
-import { useParams } from '@tanstack/react-router';
-
-export default function TodoDetail() {
-  const { todoId } = useParams();
-  return <div className="p-4">Details for todo #{todoId}</div>;
-}
