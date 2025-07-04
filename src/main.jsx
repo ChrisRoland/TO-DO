b74d2bf4f15a8e73327e353c724e98231771a52c
ChrisRoland <chrisebuberoland@gmail.com>
2025-07-04 17:58:49 +0100

    feat: Auth routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createHead, UnheadProvider } from '@unhead/react/client'
 import { ThemeProvider } from './theme-context';
+import { AuthProvider } from './contexts/AuthContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from '@tanstack/react-router';
 import router from './router';
@@ -14,9 +15,11 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UnheadProvider head={head}>
       <ThemeProvider>
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router}/>
-        </QueryClientProvider>
+        <AuthProvider>
+          <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router}/>
+          </QueryClientProvider>
+        </AuthProvider>
       </ThemeProvider>
     </UnheadProvider>
   </StrictMode>
